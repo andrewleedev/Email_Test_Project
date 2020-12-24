95037acdcec4ca8182126a6544b30ddfc3a4e57b
wbadmin <>
2020-12-24 10:15:44 -0600

    {/src/main/java/com/amsis/TrafficViolationInput.java}

--- src/main/java/com/amsis/TrafficViolationInput.java
+++ src/main/java/com/amsis/TrafficViolationInput.java
@@ -6,12 +6,44 @@ package com.amsis;
 
 public class TrafficViolationInput implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public TrafficViolationInput() {
-    }
+	private int speedLimit;
+	private int drivingSpeed;
+	private int violationPoints;
 
+	public TrafficViolationInput() {
+	}
 
+	public int getSpeedLimit() {
+		return this.speedLimit;
+	}
 
+	public void setSpeedLimit(int speedLimit) {
+		this.speedLimit = speedLimit;
+	}
+
+	public int getDrivingSpeed() {
+		return this.drivingSpeed;
+	}
+
+	public void setDrivingSpeed(int drivingSpeed) {
+		this.drivingSpeed = drivingSpeed;
+	}
+
+	public int getViolationPoints() {
+		return this.violationPoints;
+	}
+
+	public void setViolationPoints(int violationPoints) {
+		this.violationPoints = violationPoints;
+	}
+
+	public TrafficViolationInput(int speedLimit, int drivingSpeed,
+			int violationPoints) {
+		this.speedLimit = speedLimit;
+		this.drivingSpeed = drivingSpeed;
+		this.violationPoints = violationPoints;
+	}
 
 }
